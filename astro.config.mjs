82eb1afb3fc0c59729b7346b1acedf01eddc761c
luisroft <luivelasco6541@gmail.com>
2024-10-08 22:49:13 -0500

    SSR config added and language picker bg color better

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
@@ -16,4 +19,7 @@ export default defineConfig({
       en: "es",
     },
   },
+
+  output: "server",
+  adapter: vercel(),
 });
